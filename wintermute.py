83262a5c314542e18415bb0e476620a0c777bd37
Andreas Happe <andreashappe@snikt.net>
2023-04-30 21:00:16 +0200

    start with initial manual enumeration

--- wintermute.py
+++ wintermute.py
@@ -19,12 +19,19 @@ conn.connect()
 print("Get initial user from virtual machine:")
 initial_user = conn.run("whoami")
 
+# perform some simple enumeration steps first
+eumeration_cmds = create_and_ask_prompt('enumerate_first.txt', "enumeration", user=initial_user)
+for line in eumeration_cmds.split("\n"):
+    if line.startswith("- "):
+        resp = conn.run(line[1:])
+        cmd_history.append(line[1:], resp)
+
 while True:
 
     next_cmd = create_and_ask_prompt('gpt_query.txt', "next-cmd", user=initial_user, history=cmd_history.get_history())
 
-    # disable this for now, it's tragic because the AI won't tell me why it had chosen something
-    # create_and_ask_prompt("why.txt", "why", user=initial_user, history=cmd_history.dump(), next_cmd=next_cmd)
+    # disable this for now, it's tragic because the AI won't tell me why it had chosen something -> this is actually interesting: I can get the next command but not the reason why..
+    # create_and_ask_prompt("why.txt", "why", user=initial_user, history=cmd_history.get_history(), next_cmd=next_cmd)
 
     resp = conn.run(next_cmd)
     cmd_history.append(next_cmd, resp)
